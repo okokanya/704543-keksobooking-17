0edca82d7477581c2064ed1c8d3971b388c5bd42
anya <annapuskina@MacBook-Pro-anya.local>
2019-06-16 20:36:04 +0300

    need help

---
+++ js/main.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var PINNUMBER = 8;
+document.querySelector('.map').classList.remove('map--faded');
+var objectsArray = [];
+
+var getRandomNumber = function (min, max) {
+  var randomNumber = Math.random() * (max - min) + min;
+  return randomNumber;
+};
+
+var dataPins = function () {
+  for (var i = 0; i < PINNUMBER; i++) {
+    objectsArray.push({
+      'author': {
+        avatar: 'cтрока, адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются'
+      },
+      'offer': {
+        type: 'строка с одним из четырёх фиксированных значений: palace, flat, house или bungalo'
+      },
+      'location': {
+        x: getRandomNumber(0, 750),
+        y: getRandomNumber(130, 630)
+      }
+    });
+  }
+  return (objectsArray);
+};
+
+var placeForPin = document.querySelector('#pin');
+var pinTemplate = document.querySelector('.map__pin');
+
+var renderPins = function (pinsDataToRender) {
+  for (var i = 0; i < PINNUMBER; i++) {
+    var pinElement = pinTemplate.cloneNode(true);
+    pinElement.style.left = pinsDataToRender[i].location.x + 'px';
+    pinElement.style.top = pinsDataToRender[i].location.y + 'px';
+    pinElement.src = '{{pinsData.author.avatar}}';
+    pinElement.alt = '{{pinsData.offer}}';
+    placeForPin.appendChild(pinElement);
+  }
+  return pinElement;
+};
+
+var fragment = document.createDocumentFragment();
+fragment.appendChild(renderPins(dataPins()));
+document.querySelector('.map__pins').appendChild(fragment);
