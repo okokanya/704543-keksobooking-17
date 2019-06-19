83627367975e4c85bf07020292eaed95f9b8dc15
anya <annapuskina@MacBook-Pro-anya.local>
2019-06-19 21:14:20 +0300

    images and alts and more fix

--- js/main.js
+++ js/main.js
@@ -1,22 +1,56 @@
 'use strict';
 
 var PINNUMBER = 8;
+var PINHEIGHT = 70;
+
 document.querySelector('.map').classList.remove('map--faded');
-var objectsArray = [];
 
 var getRandomNumber = function (min, max) {
   var randomNumber = Math.random() * (max - min) + min;
   return randomNumber;
 };
 
+
+function generateRan(max) {
+  var random = [];
+  for (var i = 0; i < max; i++) {
+    var temp = Math.ceil(Math.random() * max);
+    if (random.indexOf(temp) === -1) {
+      random.push(temp);
+    } else {
+      i--;
+    }
+  }
+  console.log(random);
+  return random;
+}
+
+var numbArrays = generateRan(8);
+
+
+var getRandomType = function (min, max) {
+  var coeff = Math.floor(Math.random() * Math.floor(max));
+  if (coeff === 0) {
+    return 'palace';
+  } else if (coeff === 1) {
+    return 'flat';
+  } else if (coeff === 2) {
+    return 'house';
+  } else {
+    return 'bungalo';
+  }
+};
+
+var objectsArray = [];
+
 var dataPins = function () {
   for (var i = 0; i < PINNUMBER; i++) {
     objectsArray.push({
       'author': {
-        avatar: 'cтрока, адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются'
+        avatar: './img/avatars/user0' + numbArrays[i] + '.png',
       },
       'offer': {
-        type: 'строка с одним из четырёх фиксированных значений: palace, flat, house или bungalo'
+        type: (getRandomType(1, 4)),
       },
       'location': {
         x: getRandomNumber(0, 750),
@@ -30,14 +64,14 @@ var dataPins = function () {
 var pinTemplate = document.querySelector('#pin');
 var pinButton = document.querySelector('.map__pin');
 var placeForPins = document.querySelector('.map__pins');
-
+var imgPin = pinButton.querySelector('img');
 var renderPins = function (pinsDataToRender) {
   for (var i = 0; i < PINNUMBER; i++) {
     var pinElement = pinButton.cloneNode(true);
     pinElement.style.left = pinsDataToRender[i].location.x + 'px';
-    pinElement.style.top = pinsDataToRender[i].location.y + 'px';
-    pinElement.src = '{{pinsData.author.avatar}}';
-    pinElement.alt = '{{pinsData.offer}}';
+    pinElement.style.top = pinsDataToRender[i].location.y - PINHEIGHT + 'px';
+    imgPin.src = pinsDataToRender[i].author.avatar;
+    imgPin.alt = pinsDataToRender[i].offer.type;
     placeForPins.appendChild(pinElement);
   }
   return pinTemplate;
@@ -46,3 +80,4 @@ var renderPins = function (pinsDataToRender) {
 var fragment = document.createDocumentFragment();
 fragment.appendChild(renderPins(dataPins()));
 document.querySelector('.map__pins').appendChild(fragment);
+
