aa9a2279fa7fcc10080bf8828ebc727b5acc5707
anya <annapuskina@MacBook-Pro-anya.local>
2019-07-15 08:07:33 +0300

    remove first pin from renreding

---
+++ js/getPinTarget.js
@@ -0,0 +1,15 @@
+// 'use strict';
+
+// (function () {
+//   window.getPinTarget = function (obj) {
+
+//     function hide(e) {
+//       console.log(e.target);
+//     //   e.target.style.visibility = 'hidden';
+//     }
+
+//     window.getPinTarget();
+//     obj.addEventListener('click', hide, false);
+//     return window.dataInfo;
+//   };
+// })();
--- js/pin.js
+++ js/pin.js
@@ -2,13 +2,27 @@
 
 (function () {
   window.renderPins = function (pinsDataToRender) {
+    window.data = [];
+
     for (var i = 0; i < pinsDataToRender.length; i++) {
-      var pinElement = window.pinButton.cloneNode(true);
-      pinElement.style.left = pinsDataToRender[i].location.x + 'px';
-      pinElement.style.top = pinsDataToRender[i].location.y - window.PINHEIGHT + 'px';
+
+      window.pinElement = window.pinButton.cloneNode(true);
+      window.mapPinMain.remove();
+      window.pinElement.style.left = pinsDataToRender[i].location.x + 'px';
+      window.pinElement.style.top = pinsDataToRender[i].location.y - window.PINHEIGHT + 'px';
       window.imgPin.src = pinsDataToRender[i].author.avatar;
       window.imgPin.alt = pinsDataToRender[i].offer.type;
-      window.mapfield.appendChild(pinElement);
+      window.mapfield.appendChild(window.pinElement);
+      window.data.push(pinsDataToRender[i]);
+
+      console.log(window.data);
+
+      var hh = function (e) {
+        console.log(e.target);
+      };
+
+      window.pinElement.addEventListener('click', hh);
+
     }
     return window.pinTemplate;
   };
