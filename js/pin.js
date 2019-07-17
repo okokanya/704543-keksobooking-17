4b075416899144e9595ea83f1c114435451e6dcd
anya <annapuskina@MacBook-Pro-anya.local>
2019-07-17 08:50:41 +0300

    add id to pinElement

--- js/pin.js
+++ js/pin.js
@@ -3,9 +3,18 @@
 (function () {
   window.renderPins = function (pinsDataToRender) {
     window.data = [];
+    var hh = function (e) {
+      console.log(e.target);
+      console.log(e.target.id);
+      return e.target.id;
+      console.log(window.data[e.target.hh()]);
+
+      // console.log(window.data[e.target.id]);
+    };
+
+
 
     for (var i = 0; i < pinsDataToRender.length; i++) {
-
       window.pinElement = window.pinButton.cloneNode(true);
       window.pinElement.style.left = pinsDataToRender[i].location.x + 'px';
       window.pinElement.style.top = pinsDataToRender[i].location.y - window.PINHEIGHT + 'px';
@@ -13,16 +22,12 @@
       window.imgPin.alt = pinsDataToRender[i].offer.type;
       window.mapfield.appendChild(window.pinElement);
       window.data.push(pinsDataToRender[i]);
-      // window.mapPinMain.remove();
-
-
-      var hh = function (e) {
-        console.log(e.target);
-      };
+      window.pinElement.setAttribute('id', i++);
 
       window.pinElement.addEventListener('click', hh);
 
     }
     return window.pinTemplate;
   };
+
 })();
